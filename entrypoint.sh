f91c337aceeb515e1d673dea0fb4b2f46aa8ffb8
Sam Kirkland <SamKirkland@users.noreply.github.com>
2019-10-14 22:50:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,37 @@ WDEFAULT_LOCAL_DIR=${LOCAL_DIR:-"."}
 WDEFAULT_REMOTE_DIR=${REMOTE_DIR:-"."}
 WDEFAULT_ARGS=${ARGS:-""}
 WDEFAULT_METHOD=${METHOD:-"ftp"}
+WDEFAULT_SETTIMESTAMPS=${DIFFS_ONLY:-true}
+
+if $WDEFAULT_SETTIMESTAMPS; then
+	start=`date +%s`
+	IFS=$'\n'
+	list_of_files=($(git ls-files | sort))
+	unset IFS
+	
+	echo "Using Local git log to restore last_modified dates on files"
+
+	echo "|----------------------------------------------------|-------------------------------|"
+	printf "| %-50.50s | %10s |%b\n" "FileName" "New Timestamp                "
+	echo "|----------------------------------------------------|-------------------------------|"
+
+	for file in "${list_of_files[@]}"; do
+	  file_name=$(echo $file)
+
+	  ## When you collect the timestamps:
+	  TIMEPRETTY=$(date -r "$file_name")
+	  TIME=$(date -r "$file_name" -Ins)
+
+	  printf "| %-50.50s | %10s |%b\n" "$file_name" "$TIMEPRETTY"
+	  
+	  ## When you want to recover back the timestamps:
+	  touch -m -d $TIME "$file_name"
+	done
+	echo "|----------------------------------------------------|-------------------------------|"
+	end=`date +%s`
+	runtime=$((end-start))
+	echo "Updating last_modified dates took $runtime seconds"
+fi;
 
 if [ $WDEFAULT_METHOD = "sftp" ]; then
   WDEFAULT_PORT=${PORT:-"22"}
